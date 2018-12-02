5404d3824949118f1bac305f1573b108da44ab43
BarakpaevOlzhas <40425093+BarakpaevOlzhas@users.noreply.github.com>
2018-12-02 13:41:06 +0600

    Add files via upload

---
+++ AddDataBaseCollumn/AddDataBaseCollumn/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace AddDataBaseCollumn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.ButtonShowTable = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.firstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.middleName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idSele = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.idBuyer = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.idSeller = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Summ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataSele = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Buyer",
+            "Seller",
+            "Sale"});
+            this.comboBox1.Location = new System.Drawing.Point(82, 48);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Table";
+            // 
+            // ButtonShowTable
+            // 
+            this.ButtonShowTable.Location = new System.Drawing.Point(82, 142);
+            this.ButtonShowTable.Name = "ButtonShowTable";
+            this.ButtonShowTable.Size = new System.Drawing.Size(75, 23);
+            this.ButtonShowTable.TabIndex = 1;
+            this.ButtonShowTable.Text = "Show";
+            this.ButtonShowTable.UseVisualStyleBackColor = true;
+            this.ButtonShowTable.Click += new System.EventHandler(this.ButtonShowTable_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.firstName,
+            this.middleName});
+            this.dataGridView.Location = new System.Drawing.Point(290, 21);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(523, 375);
+            this.dataGridView.TabIndex = 2;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "id";
+            this.id.Name = "id";
+            // 
+            // firstName
+            // 
+            this.firstName.HeaderText = "firstName";
+            this.firstName.Name = "firstName";
+            // 
+            // middleName
+            // 
+            this.middleName.HeaderText = "middleName";
+            this.middleName.Name = "middleName";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idSele,
+            this.idBuyer,
+            this.idSeller,
+            this.Summ,
+            this.dataSele});
+            this.dataGridView1.Location = new System.Drawing.Point(290, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(523, 375);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // idSele
+            // 
+            this.idSele.HeaderText = "id";
+            this.idSele.Name = "idSele";
+            // 
+            // idBuyer
+            // 
+            this.idBuyer.HeaderText = "idBuyer";
+            this.idBuyer.Name = "idBuyer";
+            // 
+            // idSeller
+            // 
+            this.idSeller.HeaderText = "idSeller";
+            this.idSeller.Name = "idSeller";
+            // 
+            // Summ
+            // 
+            this.Summ.HeaderText = "Summ";
+            this.Summ.Name = "Summ";
+            // 
+            // dataSele
+            // 
+            this.dataSele.HeaderText = "dataSele";
+            this.dataSele.Name = "dataSele";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 408);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.ButtonShowTable);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button ButtonShowTable;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn firstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn middleName;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idSele;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idBuyer;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idSeller;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Summ;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataSele;
+    }
+}
+
---
+++ AddDataBaseCollumn/AddDataBaseCollumn/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddDataBaseCollumn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            using (SqlConnection sqlConnection = new SqlConnection("Server=localhost;Integrated security=SSPI;database=master"))
+            {
+                sqlConnection.Open();                
+
+                SqlCommand sqlCommand = new SqlCommand("create database SaleTov", sqlConnection);
+
+                try
+                {
+                    sqlCommand.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                sqlConnection.Close();
+            }
+
+            using (SqlConnection sqlConnection = new SqlConnection("Server=localhost;Integrated security=SSPI;database=SaleTov"))
+            {
+                sqlConnection.Open();
+
+                SqlCommand sqlCommand = new SqlCommand("create table Buyer(id int primary key,firstName nvarchar(MAX),middleName nvarchar(MAX))", sqlConnection);
+
+                try
+                {
+                    sqlCommand.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                sqlCommand.Connection = sqlConnection;
+
+                sqlCommand.CommandText = "create table Seller(id int primary key,firstName nvarchar(MAX), middleName nvarchar(MAX))";
+
+                try
+                {
+                    sqlCommand.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }                
+
+                sqlCommand.CommandText = "create table Sale(id int primary key,idBuyer int not null,idSeller int not null, Summ int,dateSale dateTime, foreign key(idBuyer) references Buyer(id), foreign key(idSeller) references Seller(id))";                
+
+                try
+                {
+                    sqlCommand.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                sqlCommand.CommandText = "INSERT INTO Buyer(id, firstName, middleName) Values (1,'Vasi','Tasi')";
+                sqlCommand.ExecuteNonQuery();
+                sqlCommand.CommandText = "INSERT INTO Buyer(id, firstName, middleName) Values (2,'Nurj','Kase')";
+                sqlCommand.ExecuteNonQuery();
+                sqlCommand.CommandText = "INSERT INTO Buyer(id, firstName, middleName) Values (3,'Ali','Ken')";
+                sqlCommand.ExecuteNonQuery();
+
+                sqlCommand.CommandText = "INSERT INTO Seller(id, firstName, middleName) Values (1,'Mol','Nur')";
+                sqlCommand.ExecuteNonQuery();
+                sqlCommand.CommandText = "INSERT INTO Seller(id, firstName, middleName) Values (2,'sta','Kit')";
+                sqlCommand.ExecuteNonQuery();
+                sqlCommand.CommandText = "INSERT INTO Seller(id, firstName, middleName) Values (3,'Olz','Bar')";
+                sqlCommand.ExecuteNonQuery();
+
+                sqlCommand.CommandText = "INSERT INTO Sale(id, idBuyer, idSeller, Summ, dateSale) Values (1, 1, 2, 3000, getdate())";
+                sqlCommand.ExecuteNonQuery();
+                sqlCommand.CommandText = "INSERT INTO Sale(id, idBuyer, idSeller, Summ, dateSale) Values (2, 3, 2, 4000, getdate())";
+                sqlCommand.ExecuteNonQuery();
+                sqlCommand.CommandText = "INSERT INTO Sale(id, idBuyer, idSeller, Summ, dateSale) Values (3, 2, 1, 5000, getdate())";
+                sqlCommand.ExecuteNonQuery();
+
+                sqlConnection.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonShowTable_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection("Server=localhost;Integrated security=SSPI;database=SaleTov"))
+            {
+                string command = "select * from ";
+                string tableSeller = "Seller", tableSele = "Sale", tableBuyer = "Buyer";
+
+                sqlConnection.Open();
+
+                if (comboBox1.Text.ToString() == tableSeller)
+                {
+                    SqlCommand sqlCommand = new SqlCommand(command + tableSeller, sqlConnection);
+
+                    using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                    {
+                        List<string[]> data = new List<string[]>();
+
+                        dataGridView.Rows.Clear();
+                        dataGridView.BringToFront();
+
+                        while (sqlDataReader.Read())
+                        {
+                            data.Add(new string[3]);
+                            data[data.Count - 1][0] = sqlDataReader[0].ToString();
+                            data[data.Count - 1][1] = sqlDataReader[1].ToString();
+                            data[data.Count - 1][2] = sqlDataReader[2].ToString();
+                        }
+
+                        foreach (string[] s in data)
+                            dataGridView.Rows.Add(s);
+                    }
+                }
+
+                else if (comboBox1.Text.ToString() == tableSele)
+                {
+                    SqlCommand sqlCommand = new SqlCommand(command + tableSele, sqlConnection);
+
+                    using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                    {
+                        dataGridView1.BringToFront();
+                        dataGridView1.Rows.Clear();
+
+                        List<string[]> data = new List<string[]>();
+
+                        while (sqlDataReader.Read())
+                        {
+                            data.Add(new string[5]);
+                            data[data.Count - 1][0] = sqlDataReader[0].ToString();
+                            data[data.Count - 1][1] = sqlDataReader[1].ToString();
+                            data[data.Count - 1][2] = sqlDataReader[2].ToString();
+                            data[data.Count - 1][3] = sqlDataReader[3].ToString();
+                            data[data.Count - 1][4] = sqlDataReader[4].ToString();
+                        }
+
+                        foreach (string[] s in data)
+                            dataGridView1.Rows.Add(s);
+                    }
+                }
+
+                else if (comboBox1.Text.ToString() == tableBuyer)
+                {
+                    SqlCommand sqlCommand = new SqlCommand(command + tableBuyer, sqlConnection);
+
+                    using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                    {
+                        List<string[]> data = new List<string[]>();
+
+                        dataGridView.Rows.Clear();
+                        dataGridView.BringToFront();
+
+                        while (sqlDataReader.Read())
+                        {
+                            data.Add(new string[3]);
+                            data[data.Count - 1][0] = sqlDataReader[0].ToString();
+                            data[data.Count - 1][1] = sqlDataReader[1].ToString();
+                            data[data.Count - 1][2] = sqlDataReader[2].ToString();
+                        }
+
+                        foreach (string[] s in data)
+                            dataGridView.Rows.Add(s);
+                    }
+                }
+
+                sqlConnection.Close();
+            }
+        }
+    }
+}
